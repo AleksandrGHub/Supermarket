917a40ef45238f92c696c671fdddb4c8563701e5
Matteo <t8m39rv3zy@gmail.com>
2023-08-05 09:33:34 +0300

    request

--- Supermarket/Program.cs
+++ Supermarket/Program.cs
@@ -71,7 +71,7 @@ namespace Supermarket
                     IncreaceWeight(_buyers[0].GetName(index), _buyers[0].GetWeight(index));
                     _buyers[0].DeleteCell(index);
                 }
-                Console.WriteLine();
+                Console.WriteLine("Запрос");
                 _buyers[0].ReduceMoney(_buyers[0].GetCost());
                 _money += _buyers[0].GetCost();
                 _buyers.RemoveAt(0);
